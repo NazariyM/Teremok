beea0b8177fdefee70c471226f30a3ebc2f55b2e
Nazariy Mokriy <nazarriko@gmail.com>
2016-11-11 10:12:02 +0200

    + two sect

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
-	// go js
+   // area slider
+
+    (function() {
+        var sldArea = $('.js-area'),
+            sldOutput = $('.js-area-output');
+
+        sldArea.slider({
+            range: "min",
+            value: 42,
+            min: 1,
+            max: 100,
+            slide: function(event, ui) {
+                sldOutput.val(ui.value);
+            }
+        });
+        sldOutput.val(sldArea.slider("value"));
+    })();
 
 });
